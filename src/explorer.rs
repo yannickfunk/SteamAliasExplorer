061b64b903c3369bfbf3837644371188a1ec9639
Yannick Funk <yannickfunk@yahoo.de>
2021-01-17 15:43:56 +0100

    Delete steam_user.rs

--- src/explorer.rs
+++ src/explorer.rs
@@ -2,25 +2,27 @@ use crate::STEAM_API_KEY;
 use mysql::prelude::Queryable;
 use mysql::{Params, Pool};
 use reqwest::{Client, Error};
-use serde_json::{from_str, Map, Value};
-use std::collections::VecDeque;
+use serde_json::{Map, Value};
 use std::str::FromStr;
 use std::time::SystemTime;
-use tokio::sync::broadcast::error::TryRecvError;
-use tokio::sync::broadcast::Receiver;
+use tokio::sync::broadcast::{self, error::TryRecvError, Receiver, Sender};
+use tokio::time::Duration;
 
 pub struct Explorer {
-    rx: Receiver<u64>,
-    explore_queue: VecDeque<u64>,
+    rx_external: Receiver<u64>,
+    rx_internal: Receiver<u64>,
+    tx_internal: Sender<u64>,
     conn_pool: Pool,
     client: Client,
 }
 
 impl Explorer {
-    pub fn new(rx: Receiver<u64>, conn_pool: Pool, client: Client) -> Self {
+    pub fn new(rx_external: Receiver<u64>, conn_pool: Pool, client: Client) -> Self {
+        let (tx_internal, rx_internal) = broadcast::channel(1_000_000);
         Self {
-            rx,
-            explore_queue: VecDeque::new(),
+            rx_external,
+            rx_internal,
+            tx_internal,
             conn_pool,
             client,
         }
@@ -28,138 +30,144 @@ impl Explorer {
 
     pub async fn start(&mut self) {
         loop {
-            match self.rx.try_recv() {
+            tokio::time::sleep(Duration::from_millis(100)).await;
+            match self.rx_external.try_recv() {
                 Ok(id_64) => {
                     if id_64 == 0 {
                         break;
                     } else {
-                        self.explore_queue.push_front(id_64);
+                        tokio::spawn(explore(
+                            self.conn_pool.clone(),
+                            self.client.clone(),
+                            self.tx_internal.clone(),
+                            id_64,
+                        ));
                     }
                 }
                 Err(e) => match e {
                     TryRecvError::Empty => {}
                     TryRecvError::Closed => break,
-                    TryRecvError::Lagged(_) => {
-                        println!("lagged!")
+                    TryRecvError::Lagged(e) => {
+                        log::error!("{:?}", e);
                     }
                 },
             };
-            if let Some(next_user) = self.explore_queue.pop_front() {
-                self.explore(next_user).await;
+            if let Ok(next_user) = self.rx_internal.try_recv() {
+                tokio::spawn(explore(
+                    self.conn_pool.clone(),
+                    self.client.clone(),
+                    self.tx_internal.clone(),
+                    next_user,
+                ));
             }
         }
     }
+}
 
-    async fn explore(&mut self, next_user: u64) {
-        println!("Exploring {:?}", next_user);
-        match self.get_aliases(next_user).await {
-            Ok(aliases) => {
-                let aliases = if aliases.len() == 0 {
-                    if let Ok(current_name) = self.get_current_name(next_user).await {
-                        vec![current_name]
-                    } else {
-                        println!("Could not get current name!");
-                        return;
-                    }
+async fn explore(conn_pool: Pool, client: Client, tx_internal: Sender<u64>, next_user: u64) {
+    match get_aliases(client.clone(), next_user).await {
+        Ok(aliases) => {
+            let aliases = if aliases.len() == 0 {
+                if let Ok(current_name) = get_current_name(client.clone(), next_user).await {
+                    vec![current_name]
                 } else {
-                    aliases
-                };
-                if let Err(e) = self.write_aliases_db(next_user, aliases).await {
-                    println!("Error writing aliases into database: {:?}", e);
+                    log::error!("Could not get current name for: {:?}", next_user);
+                    return;
                 }
-            }
-            Err(e) => {
-                println!(
-                    "Error retrieving aliases: {:?}, pushing back on the Queue",
-                    e
-                );
-                self.explore_queue.push_back(next_user)
-            }
-        };
-        println!("{:?}", self.explore_queue.len());
-        if self.explore_queue.len() < 1_000_000 {
-            match self.retrieve_friends(next_user).await {
-                Ok(friends) => friends
-                    .iter()
-                    .for_each(|e| self.explore_queue.push_back(*e)),
-                Err(e) => println!("Error retrieving friends: {:?}", e),
+            } else {
+                aliases
             };
-        }
-    }
-
-    async fn retrieve_friends(&mut self, id_64: u64) -> Result<Vec<u64>, Error> {
-        let request = format!("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={:}&steamid={:?}&relationship=friend", STEAM_API_KEY, id_64);
-        let text = self
-            .client
-            .get(&request)
-            .send()
-            .await?
-            .json::<Value>()
-            .await?;
-        let json: Map<String, Value> = text.as_object().unwrap().clone();
-        if let Some(friendlist) = json.get("friendslist") {
-            if let Some(friends) = friendlist.get("friends") {
-                let friend_ids: Vec<u64> = friends
-                    .as_array()
-                    .unwrap()
-                    .iter()
-                    .map(|e| u64::from_str(&e["steamid"].to_string().replace("\"", "")).unwrap())
-                    .collect();
-                return Ok(friend_ids);
+            if let Err(e) = write_aliases_db(conn_pool, next_user, aliases).await {
+                log::error!("Error writing aliases into database: {:?}", e);
             }
         }
-        println!("No public friendlist or Error retrieving friends");
-        Ok(vec![])
-    }
+        Err(e) => {
+            log::error!("Error retrieving aliases: {:?}, back on the Queue", e);
+            if let Err(e) = tx_internal.send(next_user) {
+                log::error!("Error putting user on the queue: {:?}", e)
+            }
+        }
+    };
+    match retrieve_friends(client, next_user).await {
+        Ok(friends) => friends.iter().for_each(|e| {
+            if let Err(e) = tx_internal.send(*e) {
+                log::error!("Error putting user on the queue: {:?}", e)
+            }
+        }),
+        Err(e) => log::error!("Error retrieving friends: {:?}", e),
+    };
+}
 
-    async fn get_aliases(&mut self, id_64: u64) -> Result<Vec<String>, Error> {
-        let request = format!("http://steamcommunity.com/profiles/{:}/ajaxaliases/", id_64);
-        let text = self
-            .client
-            .get(&request)
-            .send()
-            .await?
-            .json::<Value>()
-            .await?;
-        let ser_vec: Vec<Value> = text.as_array().unwrap().clone();
-        Ok(ser_vec
-            .iter()
-            .map(|e| e["newname"].to_string().replace("\"", ""))
-            .collect())
-    }
+async fn get_aliases(client: Client, id_64: u64) -> Result<Vec<String>, Error> {
+    let request = format!("http://steamcommunity.com/profiles/{:}/ajaxaliases/", id_64);
+    let text = client.get(&request).send().await?.json::<Value>().await?;
+    let ser_vec: Vec<Value> = text.as_array().unwrap().clone();
+    Ok(ser_vec
+        .iter()
+        .map(|e| e["newname"].to_string().replace("\"", ""))
+        .collect())
+}
 
-    async fn write_aliases_db(
-        &mut self,
-        id_64: u64,
-        aliases: Vec<String>,
-    ) -> Result<(), mysql::Error> {
-        let now = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs_f64();
-        let stmt = format!(
+async fn get_current_name(client: Client, id_64: u64) -> Result<String, Error> {
+    let request = format!(
+        "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={:}&steamids={:?}",
+        STEAM_API_KEY, id_64
+    );
+    let text = client.get(&request).send().await?.json::<Value>().await?;
+    let json: Map<String, Value> = text.as_object().unwrap().clone();
+    Ok(json["response"]["players"][0]["personaname"]
+        .to_string()
+        .replace("\"", ""))
+}
+
+async fn write_aliases_db(
+    conn_pool: Pool,
+    id_64: u64,
+    aliases: Vec<String>,
+) -> Result<(), mysql::Error> {
+    let query = format!("SELECT id_64 from aliases WHERE id_64 = {:?}", id_64);
+    let query_result: Vec<String> = conn_pool.get_conn()?.query(query)?;
+    let now = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs_f64();
+    if query_result.len() > 0 {
+        let update_stmt = format!(
+            "UPDATE aliases SET alias_list = '{:?}', last_written = {:?} WHERE id_64 = {:?}",
+            aliases, now, id_64
+        );
+        let _: Vec<String> = conn_pool
+            .get_conn()?
+            .exec(update_stmt.clone(), Params::Empty)?;
+        log::info!("Updated:  {:}", update_stmt);
+    } else {
+        let insert_stmt = format!(
             "INSERT INTO aliases (id_64, alias_list, last_written) VALUES ({:?}, '{:?}', {:?})",
             id_64, aliases, now
         );
-        let _: Vec<String> = self.conn_pool.get_conn()?.exec(stmt, Params::Empty)?;
-        Ok(())
+        let _: Vec<String> = conn_pool
+            .get_conn()?
+            .exec(insert_stmt.clone(), Params::Empty)?;
+        log::info!("Inserted:  {:}", insert_stmt);
     }
+    Ok(())
+}
 
-    async fn get_current_name(&mut self, id_64: u64) -> Result<String, Error> {
-        let request = format!(
-            "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={:}&steamids={:?}",
-            STEAM_API_KEY, id_64
-        );
-        let text = self
-            .client
-            .get(&request)
-            .send()
-            .await?
-            .json::<Value>()
-            .await?;
-        let json: Map<String, Value> = text.as_object().unwrap().clone();
-        Ok(json["response"]["players"][0]["personaname"]
-            .to_string()
-            .replace("\"", ""))
+async fn retrieve_friends(client: Client, id_64: u64) -> Result<Vec<u64>, Error> {
+    let request = format!("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={:}&steamid={:?}&relationship=friend", STEAM_API_KEY, id_64);
+    let text = client.get(&request).send().await?.json::<Value>().await?;
+    let json: Map<String, Value> = text.as_object().unwrap().clone();
+    if let Some(friendlist) = json.get("friendslist") {
+        if let Some(friends) = friendlist.get("friends") {
+            let friend_ids: Vec<u64> = friends
+                .as_array()
+                .unwrap()
+                .iter()
+                .map(|e| u64::from_str(&e["steamid"].to_string().replace("\"", "")).unwrap())
+                .collect();
+            return Ok(friend_ids);
+        }
     }
+    log::error!("No public friendlist or Error retrieving friends");
+    Ok(vec![])
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 mod explorer;
-
 use crate::explorer::Explorer;
+use log::{error, info, trace, warn};
 use mysql::Pool;
 use reqwest::{self, Client};
-use std::env;
-use std::str::FromStr;
+use std::time::SystemTime;
 use tokio::sync::broadcast;
-use tokio::time::sleep;
-use tokio::time::Duration;
+use tokio::time::{sleep, Duration};
 
 const STEAM_API_KEY: &str = "E564DC18D4C81A716362FC53104572D5";
 
 #[tokio::main]
 async fn main() {
+    setup_logger().unwrap();
+
     let url = "mysql://root:test@localhost:3306/alias_explorer";
     let pool = Pool::new(url).unwrap();
-    let client = Client::new();
+    let client = Client::builder().connection_verbose(false).build().unwrap();
 
     let (tx, rx) = broadcast::channel(10000);
     let mut explorer = Explorer::new(rx, pool, client);
@@ -23,8 +23,40 @@ async fn main() {
         explorer.start().await;
     };
 
-    let id_to_send_to_service = 76561198021323440;
     tokio::spawn(explorer_future);
+    let id_to_send_to_service = 76561198248077092;
     tx.send(id_to_send_to_service).unwrap();
-    sleep(Duration::from_secs(60 * 60 * 12)).await;
+    sleep(Duration::from_secs(60 * 60 * 24)).await;
+}
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(
+            fern::Dispatch::new()
+                .level(log::LevelFilter::Debug)
+                .chain(fern::log_file(format!(
+                    "log/{:?}.log",
+                    SystemTime::now()
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_secs_f64()
+                ))?),
+        )
+        .chain(
+            fern::Dispatch::new()
+                .level(log::LevelFilter::Info)
+                .chain(std::io::stdout()),
+        )
+        .apply()?;
+
+    Ok(())
 }
